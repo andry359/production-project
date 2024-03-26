962ec445bfbf883a435b4020a7e45ec357425ab1
Andry35 <uw.andrey.revtov@gmail.com>
2024-03-26 17:04:26 +0300

    add routing and lazy suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import { Counter } from "./Components/Counter";
+import React, { Suspense } from "react";
 import './index.scss';
+import { Routes } from "react-router";
+import { Route } from "react-router";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            sddfsfsdfs23e24ewa
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
