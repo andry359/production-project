f1a2be77707e9462adc58a58e10034fce2aac2b8
Andry35 <uw.andrey.revtov@gmail.com>
2024-04-04 15:31:37 +0300

    add stylelint and add i18n-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'airbnb',
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     overrides: [
@@ -27,6 +28,7 @@ module.exports = {
     },
     plugins: [
         'react',
+        'i18next',
     ],
     rules: {
         quotes: [2, 'single', { avoidEscape: true }],
@@ -56,6 +58,7 @@ module.exports = {
         ],
         'import/no-extraneous-dependencies': 'warn',
         'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,
