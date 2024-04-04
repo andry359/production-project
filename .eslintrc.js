4e4803c47e59e1dead631b386232b0d8492fe376
Andry35 <uw.andrey.revtov@gmail.com>
2024-04-05 00:03:01 +0300

    minor fix for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,6 +62,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'warn',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-length': ['error', { ignoreComments: true}]
     },
     globals: {
         __IS_DEV__: true,
