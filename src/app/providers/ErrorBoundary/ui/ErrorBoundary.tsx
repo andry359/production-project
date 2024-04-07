afb92857fa52e95e395e859d39a3108ef506cd89
Andry35 <uw.andrey.revtov@gmail.com>
2024-04-07 21:05:20 +0300

    minor fix

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -1,5 +1,4 @@
 import React, { ErrorInfo, ReactNode, Suspense } from "react";
-import { withTranslation } from "react-i18next";
 import { PageError } from "widgets/PageError";
 
 interface ErrorBoundaryProps {
@@ -13,32 +12,32 @@ interface ErrorBoundaryState {
 class ErrorBoundary 
     extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
     constructor(props: ErrorBoundaryProps) {
-      super(props);
-      this.state = { hasError: false };
+        super(props);
+        this.state = { hasError: false };
     }
-  
+
     static getDerivedStateFromError(error: Error) {
       // Update state so the next render will show the fallback UI.
-      return { hasError: true };
+        return { hasError: true };
     }
-  
+
     componentDidCatch(error: Error, errorInfo: ErrorInfo) {
       // You can also log the error to an error reporting service
-      console.log(error, errorInfo);
+        console.log(error, errorInfo);
     }
-  
+
     render() {
         const {hasError} = this.state;
         const {children} = this.props;
-      if (hasError) {
+        if (hasError) {
         // You can render any custom fallback UI
         return (
             <Suspense fallback=''>
                 <PageError />
             </Suspense>);
-      }
-  
-      return children; 
+    }
+
+        return children; 
     }
 };
 
