0dd339494003b644854ffbc88d88c218cbbea09c
Andry35 <uw.andrey.revtov@gmail.com>
2024-04-07 20:42:57 +0300

    add error and ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import { FC, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+
+// testing component for ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if(error) {
+            throw new Error();
+        };
+    }, [error]);
+    
+    return (
+        <Button
+            onClick={onThrow}
+        >
+            {t('Сгенерировать ошибку')}
+        </Button>
+    );
+};
