a4398e5405327458a53f64a6def019bf62570467
Andry35 <uw.andrey.revtov@gmail.com>
2024-04-08 23:33:30 +0300

    fix config eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,10 +61,20 @@ module.exports = {
         ],
         'import/no-extraneous-dependencies': 'warn',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true, 
+            ignoreAttribute: ['data-testid'] }],
         'max-length': ['error', { ignoreComments: true, code: 100}]
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
